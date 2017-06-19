dfda209e83c3cb8ab1a1c975a2a05b4d8c8f6ada
forelise25 <forelise25@naver.com>
2017-06-19 20:36:19 +0900

    #3 Music List in Array

--- app/src/main/java/com/example/lg/audiolist/MainActivity.java
+++ app/src/main/java/com/example/lg/audiolist/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.lg.audiolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView list;
+    Button but_play, but_stop;
+    TextView textMusic;
+    ProgressBar progress;
+    String[] musics= {"너라서 - 공유","payphone - maroon5","sugar - maroon5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list=(ListView)findViewById(R.id.list_music);
+        but_play=(Button)findViewById(R.id.but_play);
+        but_stop=(Button)findViewById(R.id.but_stop);
+        textMusic=(TextView)findViewById(R.id.text_music);
+        progress=(ProgressBar)findViewById((R.id.progress_music));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, musics);
+        list.setAdapter(adapter);
+        list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        list.setItemChecked(0,true);
     }
 }
